ce12e4470a3d0aa47023deb950b397327b949fa9
dummie106 <71605111+dummie106@users.noreply.github.com>
2021-12-15 21:10:07 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,93 @@
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>the no history search engine</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required CSS -->
+    <style>
+
+
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap');
+
+      .search {
+          text-align: center;
+          font-family: Montserrat;
+          color: #4293c2;
+      }
+
+      .empty {
+          position: relative;
+          height: 2%;
+      }
+
+      .links1 {
+          font-family: Times;
+          font-size: 25px;
+          text-align: right;
+      }
+
+      .links1 a {
+          color: #4293c2;
+          text-decoration: none;
+      }
+
+      .links1 a:hover {
+          text-decoration: underline;
+      }
+
+      @keyframes bgcolor {
+    0% {
+        background-color: #45a3e5
+    }
+
+    30% {
+        background-color: #66bf39
+    }
+
+    60% {
+        background-color: #eb670f
+    }
+
+    90% {
+        background-color: #f35
+    }
+
+    100% {
+        background-color: #864cbf
+    }
+}
+
+body {
+    -webkit-animation: bgcolor 30s infinite;
+    animation: bgcolor 20s infinite;
+    -webkit-animation-direction: alternate;
+    animation-direction: alternate;
+}
+
+        </style>
+            <!-- Required CSS -->
+</head>
+<body>
+
+<div class="empty">
+</div>
+<!--
+<div class="links1">
+<a href="#">Home</a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="#">❐ Apps</a>&nbsp&nbsp
+<br>
+<br>
+</div>
+-->
+<br>
+<br>
+<br>
+<br>
+<div class="search">
+<p><mark>what up homie</mark></p>
+</div>
+<div class="info">
+<script async src="https://cse.google.com/cse.js?cx=005845227732198661462:xl3wr8jpjog"></script>
+<div class="gcse-search"></div>
+</div>
+</body>
+</html>
