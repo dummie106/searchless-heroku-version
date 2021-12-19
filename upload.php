d9f86ae05ec84554fa1a74c232bdb170998348f1
dummie106 <71605111+dummie106@users.noreply.github.com>
2021-12-19 10:52:30 -0500

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,14 @@
+
+<?php
+$folder = "cloud";
+if (is_uploaded_file($_FILES['filename']['tmp_name']))  {  
+    if (move_uploaded_file($_FILES['filename']['tmp_name'],
+ $folder."/".$_FILES['filename']['name'])) {
+         Echo "File uploaded";
+    } else {
+         Echo "File not moved to destination folder. Check permissions";
+    };
+} else {
+     Echo "File is not uploaded.";
+};
+?>
