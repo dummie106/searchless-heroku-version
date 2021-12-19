83828d9228d72b498a147fd8ab130bbbf79d13fe
dummie106 <71605111+dummie106@users.noreply.github.com>
2021-12-19 11:06:37 -0500

    Update index.php

--- cloud/index.php
+++ cloud/index.php
@@ -1,3 +1,8 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+File: <input type="file" name="filename" />
+<input type="submit" value="Upload" />
+</form>
+
 <?php
 
 // function to get the file extension (type)
