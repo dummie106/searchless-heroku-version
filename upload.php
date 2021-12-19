a38c43aa92e894898861bac6bf0ead0753dee6e6
dummie106 <71605111+dummie106@users.noreply.github.com>
2021-12-19 11:13:49 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,3 +12,4 @@ if (is_uploaded_file($_FILES['filename']['tmp_name']))  {
      Echo "File is not uploaded.";
 };
 ?>
+<iframe src="https://searchless.herokuapp.com/cloud/" style="border:0px #ffffff none;" name="yo" scrolling="yes" frameborder="1" marginheight="0px" marginwidth="0px" height="100%" width="100%" allowfullscreen></iframe>
